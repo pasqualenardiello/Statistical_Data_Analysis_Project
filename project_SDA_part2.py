0f36e6509ace026aea5a40098c386bff8012cdac
mimmol99 <92627826+mimmol99@users.noreply.github.com>
2023-08-30 16:29:33 +0200

    Add files via upload

--- project_SDA_part2.py
+++ project_SDA_part2.py
@@ -1,31 +1,70 @@
 import scipy.io
 from scipy.io import loadmat
 import numpy as np
+import os
+from sklearn import preprocessing
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
 # 1. Caricamento dei dati
 def load_data(file_name):
 
     #if mat not already in extension,add it
+    
     appendmat = file_name.endswith(".mat")
+    file = file_name.split(".")[0].split(os.sep)[-1]
     mat_dict = loadmat(file_name,appendmat = appendmat)
     x = []
     y = []
-    for raw in mat_dict["train"]:
-        x_raw = raw[:-2]
-        y_raw = raw[-2:-1]
-        x.append(x_raw)
-        y.append(y_raw)
+
+    for raw in mat_dict[file]:
+        if file == "train":
+            x_raw = raw[:-1]
+            y_raw = raw[-1:]
+            x.append(x_raw)
+            y.append(y_raw)
+        else: 
+            x.append(raw)
+            y = None
+            
     return x,y
 
 # 2. Riduzione della dimensionalità
-def dimensionality_reduction(X,Y):
-    # Implementare il codice per la riduzione della dimensionalità
-    pass
+def dimensionality_reduction(X,min_variance=0.9):
 
-# 3. Classificatore logistico con gradiente stocastico
-def logistic_classifier(X, Y, step_size):
-    # Implementare il classificatore logistico
-    pass
+    features_standardized = standardization(X)
+    n_components = len(features_standardized[0])
+    pca_scaled = PCA(n_components)
+    pca_scaled.fit(features_standardized)
+    cumulative_variance_scaled = np.cumsum(pca_scaled.explained_variance_ratio_)
+    plot_variance(pca_scaled.explained_variance_ratio_,cumulative_variance_scaled, np.arange(n_components), 'Principal components scaled', 'Variance ratio','variance on features standardized')
+
+    # Find the number of components for at least min variance
+    n_components_min_variance = np.argmax(cumulative_variance_scaled >= min_variance) + 1
+    # Implementare il codice per la riduzione della dimensionalità
+    pca_reduced = PCA(n_components_min_variance)
+    features_reduced = pca_reduced.fit_transform(features_standardized)
+
+    return features_standardized
+    
+
+def standardization(features):
+        
+    standard_scaler = preprocessing.StandardScaler()
+    scaler_trained = standard_scaler.fit(features)
+    features_standardized = scaler_trained.transform(features)
+
+    return features_standardized
+
+def plot_variance(variance, cum_variance, interval, x_label, y_label,title):
+    plt.bar(interval, variance, alpha = 0.5, align='center', label = 'Individual variance')
+    plt.step(interval, cum_variance, where='mid', label = 'Cumulative variance')
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.legend(loc='best')
+    plt.show()
+
 
 # 4. Valutazione dello step-size
 def evaluate_step_size():
@@ -50,12 +89,14 @@ def to_ascii(binary_bytes):
 if __name__ == "__main__":
     
     #load data
+    print("Train data\n")
     train_file_name = "Gruppo 3/train.mat"
-    test_file_name = "Gruppo 3/test.mat"
     x_train,y_train = load_data(file_name=train_file_name)
-    x_test,y_test = load_data(file_name=train_file_name)
+    print("Test data\n")
+    test_file_name = "Gruppo 3/test.mat"
+    x_test,y_test = load_data(file_name=test_file_name)
     #reduce dimensionality
-    x_train_reduced = dimensionality_reduction(x_train,y_train)
+    x_train_reduced = dimensionality_reduction(x_train)
     #fit classifier
     logistic_classifier = logistic_classifier(x_train_reduced,y_train)
     #predictions on test data
