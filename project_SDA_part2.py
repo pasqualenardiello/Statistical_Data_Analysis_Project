c0efa5b05440ac0e3ba56a54bf88ebce8ea34eae
mimmol99 <92627826+mimmol99@users.noreply.github.com>
2023-09-01 17:58:06 +0200

    Add files via upload

--- project_SDA_part2.py
+++ project_SDA_part2.py
@@ -17,7 +17,6 @@ def load_data(file_name):
     mat_dict = loadmat(file_name,appendmat = appendmat)
     x = []
     y = []
-    tmp = []
 
     for raw in mat_dict[file]:
         if file == "train":
@@ -29,8 +28,7 @@ def load_data(file_name):
             x.append(raw[:-1])
             y = None
             t_raw = raw[-1:]
-            tmp.append(t_raw[0])
-    print('True time series', tmp)
+            
     print('Features vectors', len(x))
     print('Features per array', len(x[0]))
     if y is not None:
@@ -47,7 +45,32 @@ def load_data(file_name):
     return x,y
 
 #Riduzione della dimensionalità
-def dimensionality_reduction(X,file_path,min_variance=0.9):
+
+def correlation_dimensionality_reduction(x, min_corr=0.75):
+
+    X = pd.DataFrame.from_dict(x)
+    # Step 1: Compute the correlation matrix
+    corr_matrix = X.corr().abs()
+    
+    # Step 2: Identify correlated variables
+    upper = corr_matrix.where(pd.DataFrame(np.triu(np.ones(corr_matrix.shape), k=1)).astype(bool))
+    to_drop = [col for col in upper.columns if any(upper[col] > min_corr)]
+    
+    # Step 3: Remove correlated variables
+    X_reduced = X.drop(columns=to_drop)
+    
+    # Print information about deleted variables
+    print(f"After correlation deleted {len(to_drop)} variables: {to_drop}")
+    
+    return X_reduced,to_drop
+
+def drop_columns(x,columns_to_drop):
+    X = pd.DataFrame.from_dict(x)
+    X_reduced = X.drop(columns=columns_to_drop)
+    return X_reduced
+
+
+def pca_dimensionality_reduction(X,file_path,min_variance=0.90):
     features_standardized = standardization(X)
     n_components = len(features_standardized[0])
     pca_scaled = PCA(n_components)
@@ -56,52 +79,42 @@ def dimensionality_reduction(X,file_path,min_variance=0.9):
     plot_variance(pca_scaled.explained_variance_ratio_,cumulative_variance_scaled, np.arange(n_components), 'Principal components scaled', 'Variance ratio','variance on features standardized',file_path=file_path)
     # Find the number of components for at least min variance
     n_components_min_variance = np.argmax(cumulative_variance_scaled >= min_variance) + 1
+    print(f"After PCA deleted {n_components-n_components_min_variance}")
     # Implementare il codice per la riduzione della dimensionalità
     pca_reducer = PCA(n_components_min_variance)
     features_reduced = pca_reducer.fit_transform(features_standardized)
     return features_reduced,pca_reducer
-    
+
+
 def standardization(features): 
     standard_scaler = preprocessing.StandardScaler()
     scaler_trained = standard_scaler.fit(features)
     features_standardized = scaler_trained.transform(features)
     return features_standardized
 
+
 def plot_variance(variance, cum_variance, interval, x_label, y_label,title,file_path):
     
     plt.bar(interval, variance, alpha = 0.5, align='center', label = 'Individual variance')
     plt.step(interval, cum_variance, where='mid', label = 'Cumulative variance')
     plt.title(title)
+    plt.grid(True)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.legend(loc='best')
     plt.savefig(file_path)
     plt.clf()
 
+
 # Sigmoid function
 def sigmoid(z):
     z = np.clip(z, -500, 500)
     return 1 / (1 + np.exp(-z))
 
+
 def compute_gradient(y, y_pred, X):
     return np.dot(X.T, np.subtract(y_pred,y)) / len(y)
 
-'''
-def logistic_class_SGD(X, y, lr=0.01, n_epochs=10):
-    # Aggiungi un bias a X
-    X = np.insert(X, 0, 1, axis=1)
-    m, n = X.shape
-    weights = np.random.randn(n)
-    for epoch in tqdm(range(n_epochs)):
-        for i in range(m):
-            random_idx = np.random.randint(m)
-            xi = X[random_idx:random_idx+1]
-            yi = y[random_idx:random_idx+1]
-            y_pred = sigmoid(np.dot(xi, weights))
-            gradient = compute_gradient(yi, y_pred, xi)
-            weights -= lr * gradient
-    return weights
-'''
 
 def logistic_class_SGD(X, y, lr=0.01, n_epochs=10, file_path='gradient_vs_epoch.png'):
     X = np.insert(X, 0, 1, axis=1)
@@ -127,6 +140,7 @@ def logistic_class_SGD(X, y, lr=0.01, n_epochs=10, file_path='gradient_vs_epoch.
     
     plot_gradients(epoch_avg_gradients, file_path)
     return weights
+
 
 def plot_gradients(gradient_norms, file_path):
     plt.plot(range(len(gradient_norms)), gradient_norms, marker='o')
@@ -161,7 +175,7 @@ def evaluate_step_sizes(X, y,step_sizes,epochs,file_path):
         pred, beta = logistic_classifer(X, y, step_size=step_size,epochs=epochs,file_path=os.path.join(images_path,'gradient_epochs_with_step_size_'+str(step_size)+'.png'))
         vett = [1 if pred[i]==y[i] else 0 for i in range(len(y))]
         accuracy = sum(vett)/len(vett)
-        print('Accuracy for', step_size, accuracy)
+        print(f"Accuracy for step size {step_size}: {round(accuracy,3)}")
 
         accuracies.append(round(accuracy,3))
 
@@ -238,7 +252,6 @@ def plot_correlation_matrix(x,filename):
 
 if __name__ == "__main__":
     
-
     images_path = os.path.join(os.getcwd(),'Images')
     if os.path.exists(images_path) is False:
         os.makedirs(images_path)
@@ -253,24 +266,36 @@ if __name__ == "__main__":
     test_file_name = "Gruppo 3/test.mat"
     x_test,y_test = load_data(file_name=test_file_name)
 
-    plot_correlation_matrix(x_train,filename=os.path.join(images_path,'correlation_matrix.png'))
+    plot_correlation_matrix(x_train,filename=os.path.join(images_path,'correlation_matrix_before_dim_reduction.png'))
     
     print('==============================================')
+    print("Dimensionality Reduction")
+    print("Correlation ")
     #reduce dimensionality
-    x_train_reduced,pca_reducer = dimensionality_reduction(x_train,file_path=os.path.join(images_path,'dimensionality_reduction.png'))
-    print("Features arrays reduced:", len(x_train_reduced))
-    print("Features per train array reduced:", len(x_train_reduced[0]))
+    x_corr_reduced,columns_dropped= correlation_dimensionality_reduction(x_train)
+    x_test_corr_reduced = drop_columns(x_test,columns_dropped)
+    print("Features arrays reduced:", len(x_corr_reduced))
+    print("Features per train array reduced:", x_corr_reduced.shape[1])
+    plot_correlation_matrix(x_corr_reduced,filename=os.path.join(images_path,'correlation_matrix_after_corr_reduction.png'))
+    print("PCA")
+    x_corr_pca_reduced,pca_reducer = pca_dimensionality_reduction(x_corr_reduced,file_path=os.path.join(images_path,'dimensionality_reduction.png'))
+    
+    plot_correlation_matrix(x_corr_pca_reduced,filename=os.path.join(images_path,'correlation_matrix_after_corr_and_pca_reduction.png'))
+    
+    print("Features arrays reduced:", len(x_corr_pca_reduced))
+    print("Features per train array reduced:", len(x_corr_pca_reduced[0]))
     print('==============================================')
+    print('Test')
     #step sizes
     step_sizes = [0.00001,0.0001,0.001, 0.01, 0.1, 1]
     #find best logistic classifier
-    best_bias,best_beta = evaluate_step_sizes(x_train_reduced,y_train,step_sizes,epochs = 5,file_path=os.path.join(images_path,'accuracy on step size.png'))
+    best_bias,best_beta = evaluate_step_sizes(x_corr_pca_reduced,y_train,step_sizes,epochs = 10,file_path=os.path.join(images_path,'accuracy on step size.png'))
     best_beta = best_beta[1:]
     #print(f"best beta:{best_beta}")
-    x_test_reduced = pca_reducer.transform(x_test)
-    print("Lenght x test reduced:",len(x_test_reduced[0]))
+    x_test_corr_pca_reduced = pca_reducer.transform(x_test_corr_reduced)
+    print("Lenght x test reduced:",len(x_test_corr_pca_reduced[0]))
     #predictions on test data
-    predictions = logistic_inference(x_test_reduced,best_beta)
+    predictions = logistic_inference(x_test_corr_pca_reduced,best_beta)
     #print(predictions)
     #convert predictions to binary
     binary_bytes = decision_rule(predictions)
